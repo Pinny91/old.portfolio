6860c78fdc49cec40d6369afb489d63aef290fcd
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2016-03-16 17:51:12 +0100

    Added files via upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-jQuery(document).ready(function() {
-  jQuery('#small-menu').click(function(e) {
-    jQuery(this).toggleClass('active');
-    jQuery('#nav-bar ul').toggleClass('active');
-    e.preventDefault();
-  });
+var eventFired = 1;
+if ($(window).width() < 760) {
+	eventFired = 0;
+}
+else {
+    eventFired = 1;
+}
+$(window).on('resize', function() {
+if ($(window).width() < 760) {
+	eventFired = 0;
+}
+else {
+    eventFired = 1;
+}
 });
+    if (eventFired == 0) {       
+			$("#small-menu").on("click", function (e) {
+				$("#nav-bar ul").toggleClass("active");
+				$(this).toggleClass("active");
+				e.preventDefault();
+			});
+			$("#nav-bar ul").on("click", ".nav-link", function () {
+				//$("#nav").slideToggle();
+				//alert('fuck you ' + eventFired);
+				$("#small-menu").click();
+			});
+        
+    } 
+	else {
+		$(this).removeClass("active");
+	}	
