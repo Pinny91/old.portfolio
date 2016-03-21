c882290e932e978e7b53d50d564743eae36969e3
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2016-03-21 19:11:51 +0100

    Added files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,9 @@
 jQuery(document).ready(function(){
   jQuery("#nav-bar").click(function() {
 	   jQuery("#small-menu").toggleClass("active");
-	   jQuery("#nav-bar ul").toggleClass("active");
+	   jQuery("#nav-list ul").toggleClass("active");
 		//e.preventDefault();
-		jQuery(".nav-link").click(function(e) {
-		if ("#nav-bar".hasClass('active')) {
-			"#nav-bar".removeClass('active');
-			alert("Boe");
-		}
-		
-  });
+
   	
 
  });
